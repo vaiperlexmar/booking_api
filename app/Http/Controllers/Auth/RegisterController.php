dc429bf59beb2b3936abe94196da2150bedbda22
vaiperlexmar <vaiperlexmar@gmail.com>
2025-06-04 12:29:13 +0300

    hotfix: убрана опечяткя

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -23,7 +23,7 @@ class RegisterController extends Controller
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
         ]);
-        ~
+
         $user = User::create([
             'name' => $validateData['name'],
             'email' => $validateData['email'],
