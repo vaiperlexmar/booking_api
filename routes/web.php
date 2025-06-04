238633a55a0596e4b0dd481aef80bc77f220f615
vaiperlexmar <vaiperlexmar@gmail.com>
2025-06-04 14:13:53 +0300

    bugfix: Убрана возможность заходить на страницу регистрации авторизированных пользователей

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
-Route::get('/login', [RegisterController::class, 'login'])->name('login');
+Route::get('/register', [RegisterController::class, 'index'])->name('register')->middleware('guest');
+Route::get('/login', [RegisterController::class, 'login'])->name('login')->middleware('guest');
 Route::post('api/v1/auth/register', [RegisterController::class, 'store'])->name('api.v1.register');
 
